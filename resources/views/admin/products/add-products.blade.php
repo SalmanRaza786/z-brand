baa5a32cb3495e2592dd2299f455f362a5584574
Salman Raza Akram <salmanrazabwn@gmail.com>
2022-02-24 20:32:13 +0500

    some changes

--- resources/views/admin/products/add-products.blade.php
+++ resources/views/admin/products/add-products.blade.php
@@ -44,86 +44,68 @@
         <div class="card">
             <div class="card-body">
 
-                <form method="post" action="{{url("new-employee")}}" class="needs-validation" novalidate id="regForm" enctype="multipart/form-data">
+
+                <form action="{{ url('add-products') }}" method="post" enctype="multipart/form-data">
                     @csrf
-                    <div class="card tab">
-                        <div class="card-header">
-                            <h4 class="card-title mb-0">Personal Information</h4>
-                        </div>
-                        <div class="card-body">
 
 
-
-
-                            <div class="row">
-                                <div class="form-group col-sm-6">
-                                    <label>Name:<span class="text-danger">*</span></label>
-                                    <input class="form-control" type="number" name="cnic" placeholder="Name" required>
-                                </div>
-                                <div class="form-group col-sm-6">
-                                    <label>Price <span class="text-danger">*</span></label>
-                                    <input class="form-control" type="number" name="phone" placeholder="Price" required>
-                                </div>
-
-                                <div class="form-group col-sm-6">
-                                    <label>Sale Price <span class="text-danger">*</span></label>
-                                    <div class="cal-"> <input class="form-control " type="text" name="dob" placeholder="Sale Proce" required></div>
-                                </div>
-
-
-                                <div class="form-group col-sm-6">
-                                    <label>Description <span class="text-danger">*</span></label>
-                                    <div class="cal-">
-                                        <textarea name="" id="" cols="10" rows="3" class="form-control"></textarea>
-                                    </div>
-                                </div>
-                            </div>
-
-
-                            <div class="row">
-                                <div class="form-group col-sm-6">
-                                    <label>Cover Image<span class="text-danger">*</span></label>
-                                    <input type="file" name="file" class="form-control" onchange="previewFile(this);">
-
-                                </div>
-
-                                <div class="form-group col-sm-6">
-                                    <div class="input-group-btn">
-                                        <label for=""></label>
-                                        <button class="btn btn-success" type="button"><i class="glyphicon glyphicon-plus" style="margin-top: 20px"></i>Add</button>
-                                    </div>
-                                </div>
-
-                            </div>
-
-
-                            <div class="row">
-
-
-                            
-                                    <div class="clone hide">
-                                        <div class="control-group input-group" style="margin-top:10px">
-                                            <input type="file" name="images[]" class="form-control">
-                                            <div class="input-group-btn">
-                                                <button class="btn btn-danger" type="button"><i class="glyphicon glyphicon-remove"></i> Remove</button>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="col-md-2">
-                                </div>
+<div class="row">
+                        <div class="col-md-6">
+                            <div class="d-flex flex-row">
+                                <label for=""> Name</label>
+                                <span class="text-danger">*</span>
+                                <input type="text" class="form-control ml-5" name="name">
                             </div>
                         </div>
+
+                        <div class="col-md-6">
+                            <div class="d-flex flex-row">
+                                <label for=""> Price</label>
+                                <span class="text-danger">*</span>
+                                <input type="text" class="form-control ml-5" name="name">
+                            </div>
+                        </div>
+
+                        <div class="col-md-6">
+                            <div class="d-flex flex-row">
+                                <label for=""> Price</label>
+                                <span class="text-danger">*</span>
+                                <input type="text" class="form-control ml-5" name="name">
+                            </div>
+                        </div>
+
+                        <div class="col-md-6">
+                            <div class="d-flex flex-row">
+                                <label for=""> Price</label>
+                                <span class="text-danger">*</span>
+                                <input type="text" class="form-control ml-5" name="name">
+                            </div>
+                        </div>
+</div>
+
+
+
+
+                    <div class="row mt-1">
+                        <!-- tabel start -->
+
+
+                        <table class="table table-bordered mt-5 table-style">
+
+
+                            <tbody id="tblPurchase">
+                            <tr>
+
+                                <td>
+                                    <label for="">Cover Image</label>
+                                    <input type="file" class="form-control " name="cover_image" required></td>
+                                <td><button type="button" class="btn-success" id="addNewRow"><i class="fa fa-plus"></i></button> </td>
+                            </tr>
+                            </tbody>
+
+                        </table>
+                        <button class="btn btn-primary" id="btnSubmit">Submit</button>
                     </div>
-
-
-                    <div style="overflow:auto;" class="btn-group-div">
-                        <div style="float:right;">
-
-                            <button type="submit" class="btn btn-success" id="nextBtn">Save</button>
-                        </div>
-                    </div>
-
                 </form>
 
             </div>
@@ -132,20 +114,97 @@
     </div>
 
 
-
     <script type="text/javascript">
-        $(document).ready(function() {
-
-            $(".btn-success").click(function(){
-                var html = $(".clone").html();
-                $(".increment").after(html);
+        $(function () {
+            $('#addNewRow').on('click', function () {
+                var tr = $("#dvOrders").find("Table").find("TR:has(td)").clone();
+                console.log(tr);
+                $("#tblPurchase").append(tr);
             });
-
-            $("body").on("click",".btn-danger",function(){
-                $(this).parents(".control-group").remove();
-            });
-
         });
-
     </script>
+
+    <div id="dvOrders" style="display:none">
+
+        <table class="table table-bordered mt-5 table-style secondtable " >
+
+            <tr>
+
+                <td>
+                    <label for="">Images</label>
+                    <input type="file" class="form-control "  name="images[]" ></td>
+                <td style="color:red;cursor: pointer" class="delete-row" title="Remove"><i class="fa fa-trash"></i></td>
+            </tr>
+        </table>
+    </div>
+    <script>
+        $(document).ready(function () {
+
+            // Denotes total number of rows
+            var rowIdx = 0;
+
+
+            // jQuery button click event to remove a row.
+            $('#tblPurchase').on('click', '.delete-row', function () {
+
+                // Getting all the rows next to the row
+                // containing the clicked button
+                var child = $(this).closest('tr').nextAll();
+
+                // Iterating across all the rows
+                // obtained to change the index
+                child.each(function () {
+
+                    // Getting <tr> id.
+                    var id = $(this).attr('id');
+
+                    // Getting the <p> inside the .row-index class.
+                    var idx = $(this).children('.row-index').children('p');
+
+                    // Gets the row number from <tr> id.
+                    var dig = parseInt(id.substring(1));
+
+                    // Modifying row index.
+                    idx.html(`Row ${dig - 1}`);
+
+                    // Modifying row id.
+                    $(this).attr('id', `R${dig - 1}`);
+                });
+
+                // Removing the current row.
+                $(this).closest('tr').remove();
+
+                // Decreasing total number of rows by 1.
+                rowIdx--;
+            });
+
+
+
+
+            $('.livesearch').select2({
+
+                ajax: {
+                    url: '{{url("get-clients-name")}}',
+                    dataType: 'json',
+                    delay: 250,
+                    processResults: function (data) {
+
+                        return {
+                            results: $.map(data, function (item) {
+                                return {
+                                    text: item.name,
+                                    id: item.id
+                                }
+                            })
+                        };
+                    },
+                    cache: true
+                }
+            });
+        });
+    </script>
+
+
 @endsection
+
+
